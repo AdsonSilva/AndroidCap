1a34587a9fbc4718f9d8a03844acb0ca5329c858
AdsonSilva <adson.silva@ccc.ufcg.edu.br>
2016-09-08 19:34:21 -0300

    UserForm - Aula 4

---
+++ UserFormApp/app/src/main/java/com/example/huawei/userFormApp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.huawei.userFormApp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.huawei.exemploaula4.R;
+
+/**
+ * Created by huawei on 08/09/16.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
