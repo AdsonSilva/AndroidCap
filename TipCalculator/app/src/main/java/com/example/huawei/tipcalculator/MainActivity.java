6c12d742ad55752afef82a9a4d90d470b788588c
AdsonSilva <adson.silva@ccc.ufcg.edu.br>
2016-09-12 18:39:22 -0300

    Adição de quantidade de pessoas

--- TipCalculator/app/src/main/java/com/example/huawei/tipcalculator/MainActivity.java
+++ TipCalculator/app/src/main/java/com/example/huawei/tipcalculator/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.huawei.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar gorjeta = (SeekBar) findViewById(R.id.seekPercent);
+        final TextView textGorjeta = (TextView) findViewById(R.id.seekText);
+
+        gorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+                textGorjeta.setText(progress + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                textGorjeta.setText(progress + "%");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textGorjeta.setText(progress + "%");
+            }
+
+        });
+
     }
 }
